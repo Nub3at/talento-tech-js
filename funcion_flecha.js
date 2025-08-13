2c8c97b5815f40562daac505939cbff1dc3ec3dc
siuldevplus <luiscastrillondiaz@outlook.com>
2025-08-12 19:30:45 -0500

    creamos js y vinculamos

---
+++ funcion_flecha.js
@@ -0,0 +1,14 @@
+function nombreDeLaFuncion(n1,n2){
+    console.log("Los numeros son: ", n1, n2)
+}
+
+const variable = () =>{
+
+}
+
+const imprimirNomrbe = (nombre)=>{
+    return "Hola " + nombre + "!";
+}
+
+resultado = imprimirNomrbe("Miguel")
+console.log(resultado)
