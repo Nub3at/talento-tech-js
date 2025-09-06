b72af57757a1d26995206760d15ca729765e6c49
Nub3at <miguel097482@gmail.com>
2025-09-06 14:37:02 -0500

    corrigiendo funcionalidad de los botones

--- DOM/main.js
+++ DOM/main.js
@@ -4,10 +4,10 @@ const v3 = "Un gran poder, conlleva una gran responsabilidad.";
 const v4 = "Ten cuidado con el duende verde";
 
 const spiderman = (peligro) => {
-    if(peligro = true){
+    if(peligro == true){
         return `Como decia el ${v1}, ${v2}: "${v3}"`;
-    }else{
-        return `Como decia el ${v1} ${v4}`;
+    }if(peligro == false){
+        return `Como decia el ${v1}: "${v4}"`;
     }
 };
 
@@ -18,20 +18,30 @@ document.body.appendChild(titulo);
 
 //crear un boton desde JS
 const boton = document.createElement("button");
-const boton2 = document.createElement("button");
+const boton_1 = document.createElement("button");
 boton.innerText = "Peligro";
-boton2.innerText = "A salvo";
+boton_1.innerText = "A salvo";
 document.body.appendChild(boton);
-document.body.appendChild(boton2);
+document.body.appendChild(boton_1);
 
 
 //enventos en JS con botones
 boton.addEventListener("click",()=>{
-    peligro = false;
-    recordando = spiderman(peligro);
+    var_pel = false;
+    recordando = spiderman(var_pel);
     //window.location.href = "index.html";
     document.body.innerHTML=`
     <h1>${recordando}</h1>
     <button id='boton2'><a href='index.html'>Volver</a></button>
     `;
+})
+
+boton_1.addEventListener("click",()=>{
+    var_pel_2 = true;
+    recordando = spiderman(var_pel_2);
+    //window.location.href = "index.html";
+    document.body.innerHTML=`
+    <h1>${recordando}</h1>
+    <button id='boton2'><a href='index.html'>Volver</a></button>
+       `;
 })
